771e89e1c2aa70f3d1d2a79f8e72e6b3221d561c
Zebulun Arendsee <arendsee@iastate.edu>
2016-07-31 19:57:51 -0500

    Clean and document test script

--- test/runtests.sh
+++ test/runtests.sh
@@ -6,8 +6,14 @@ synder=$PWD/synder
 total_passed=0
 total_failed=0
 
+announce(){
+    [[ -t 1 ]] && o="\e[1;33m$1\e[0m" || o=$1
+    echo -e $o
+}
+
 warn(){
-    echo -e "\e[1;31m$1\e[0m"
+    [[ -t 1 ]] && o="\e[1;31m$1\e[0m" || o=$1
+    echo -e $o
 }
 
 emphasize(){
@@ -16,7 +22,8 @@ emphasize(){
 }
 
 emphasize_n(){
-    echo -ne "\e[01;39m$1\e[0m"
+    [[ -t 1 ]] && o="\e[1;39m$1\e[0m" || o=$1
+    echo -ne $o
 }
 
 runtest(){
@@ -52,29 +59,42 @@ runtest(){
 }
 
 #---------------------------------------------------------------------
-dir="$PWD/test/A"
-runtest $dir hi     "itree (set A - hi)"
-runtest $dir within "itree (set A - within)"
-runtest $dir lo     "itree (set A - lo)"
+dir="$PWD/test/test-data/one-block"
+announce "\nTesting with synteny map length == 1"
+runtest $dir hi     "query downstream of block"
+runtest $dir within "query within block"
+runtest $dir lo     "query upstream of block"
 
 #---------------------------------------------------------------------
-dir="$PWD/test/B"
-runtest $dir hi "itree (set B - hi)"
-runtest $dir within "itree (set B - within)"
-runtest $dir lo "itree (set B - lo)"
+dir="$PWD/test/test-data/two-block"
+announce "\nTesting with synteny map length == 2"
+runtest $dir hi      "query downstream of all blocks"
+runtest $dir between "query between two blocks"
+runtest $dir lo      "query upstream of all blocks"
 
 #---------------------------------------------------------------------
-dir="$PWD/test/C"
-runtest $dir a "itree (set C - a)"
-runtest $dir b "itree (set C - b)"
-runtest $dir c "itree (set C - c)"
-runtest $dir d "itree (set C - d)"
-runtest $dir e "itree (set C - e)"
-runtest $dir f "itree (set C - f)"
-runtest $dir g "itree (set C - g)"
-runtest $dir h "itree (set C - h)"
-runtest $dir i "itree (set C - i)"
-runtest $dir j "itree (set C - j)"
+dir="$PWD/test/test-data/multi-block"
+announce "\nTesting with 5 adjacent blocks on the same strand"
+runtest $dir a "Extreme left"
+runtest $dir b "Inbetween two adjacent blocks"
+runtest $dir c "Starts inbetween adjacent blocks"
+runtest $dir d "Stops inbetween adjacent blocks"
+runtest $dir e "Inbetween two adjacent blocks"
+runtest $dir f "Starts before block 3, ends after block 3"
+runtest $dir g "Starts in block 2, ends after block 3"
+runtest $dir h "Starts before block 2, ends after block 3"
+runtest $dir i "Starts in block 2, ends in block 2"
+runtest $dir j "Extreme right"
+
+#---------------------------------------------------------------------
+dir="$PWD/test/test-data/simple-duplication"
+announce "\nTest simple tandem duplication"
+runtest $dir between "Query starts between the duplicated intervals"
+
+#---------------------------------------------------------------------
+dir="$PWD/test/test-data/one-interval-inversion"
+announce "\nTest when a single interval is inverted"
+runtest $dir between "Query next to inverted interval"
 
 #---------------------------------------------------------------------
 echo
@@ -86,12 +106,12 @@ valgrind_checked=0
 valgrind_exit_status=1
 if hash valgrind 2> /dev/null; then
     valgrind_checked=1
-    dir="$PWD/test/C"
+    dir="$PWD/test/test-data/multi-block"
     tmp=/tmp/synder-$RANDOM
     mkdir $tmp
     $synder -d $dir/map.syn a b $tmp/db 
     valgrind $synder \
-        -i "$PWD/test/C/c.gff" \
+        -i "$PWD/test/test-data/multi-block/c.gff" \
         -s $tmp/db/a_b.txt \
         -c search > /dev/null 2> valgrind.log
     valgrind_exit_status=$?
@@ -111,7 +131,7 @@ else
     if [[ $valgrind_exit_status == 0 ]]
     then
         emphasize_n "valgrind pure"
-        echo " (for synder search of C/c.gff against C/map.syn)"
+        echo " (for synder search of multi-block/c.gff against multi-block/map.syn)"
         rm valgrind.log
     else
         warn "valgrind failed - see valgrind.log"
