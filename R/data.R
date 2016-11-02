1095e07bd940fb5dcdea27abfa4d00084226885a
Zebulun Arendsee <arendsee@iastate.edu>
2016-11-02 13:30:05 -0500

    Add toy dataset

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Toy dataset for synder
+#'
+#' Contains 4 files:
+#' 1. synmap     - hand made synteny map
+#' 2. query_gff  - set of intervals relative to the query
+#' 3. target_gff - set of intervals relative to the target
+#' 4. hitmap     - map to filter through the main synteny map
+#' 
+#' @format List
+"synder_toy"
