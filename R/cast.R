1b322d2cc93b51334dda49a65df523d31fbad58b
Zebulun Arendsee <arendsee@iastate.edu>
2017-06-30 14:24:28 -0500

    Add cast functions
    These are functions such as `as_synmap` that convert something that can
    be cast as a data.frame to the appropriate synder type. The `as_*`
    family does full checking. The `read_*` family is now uses `as_*` for
    checking.
    
    I ought to add handling for converting to GFF from a GRanges
    bioconductor object.

---
+++ R/cast.R
@@ -0,0 +1,92 @@
+#' Synder cast functions
+#'
+#' cast data as Synder objects
+#'
+#' @param d input type
+#' @name synder_cast
+NULL
+
+
+#' @rdname synder_cast
+#' @export
+as_synmap <- function(d) {
+  d <- as.data.frame(d)
+
+  # assert correct number of columns
+  stopifnot(ncol(d) == 8)
+
+  # Set column names
+  names(d) <- names(SYNMAP_COLS)
+
+  # Assert column types match expectations
+  stopifnot(SYNMAP_COLS == lapply(d, class))
+
+  # Assign class membership
+  class(d) <- append('synmap', class(d))
+
+  return(d)
+}
+
+#' @rdname synder_cast
+#' @export
+as_gff <- function(d) {
+  d <- as.data.frame(d)
+
+  # Assert the correct number of columns were read
+  stopifnot(ncol(d) == 9)
+
+  # Set column names
+  names(d) <- names(GFF_COLS)
+
+  # Set types (readr guesses the others correctly)
+  d$score <- as.numeric(d$score)
+  d$phase <- as.integer(d$phase)
+
+  # Assert column types match expectations
+  stopifnot(GFF_COLS == lapply(d, class))
+
+  # Assign class membership
+  class(d) <- append('gff', class(d))
+
+  return(d)
+}
+
+#' @rdname synder_cast
+#' @export
+as_hitmap <- function(d) {
+  d <- as.data.frame(d)
+
+  # Assert the correct number of columns were read
+  stopifnot(ncol(d) >= 6)
+
+  # Set column names
+  names(d)[1:6] <- names(SYNMAP_COLS)[1:6]
+
+  # Assert column types match expectations
+  stopifnot(SYNMAP_COLS[1:6] == lapply(d[,1:6], class))
+
+  # Assign class membership
+  class(d) <- append('hitmap', class(d))
+
+  return(d)
+}
+
+#' @rdname synder_cast
+#' @export
+as_conlen <- function(d) {
+  d <- as.data.frame(d)
+
+  # Assert the correct number of columns were read
+  stopifnot(ncol(d) == 2)
+
+  # Set column names
+  names(d) <- names(CON_LENGTH)
+
+  # Assert column types match expectations
+  stopifnot(CON_LENGTH == lapply(d, class))
+
+  # Assign class membership
+  class(d) <- append('conlen', class(d))
+
+  return(d)
+}
