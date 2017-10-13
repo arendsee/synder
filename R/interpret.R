59df650085f6c908dbf55c179f70e6058ba437eb
Zebulun Arendsee <arendsee@iastate.edu>
2017-10-13 02:30:04 -0500

    Write flag_summary function
    This was adapted from Fagin. I should rethink the approach, though.
    Specifically, unbound is a bit fishy, since it does not distinguish
    between cases where a link is between the unbound edges.
    
    It might be better if the boolean was replaced with count, specifying
    how many elements fall into each category.
    
    Also, the `unassembled` and `scrambled` terms could be added to the
    summaries.
    
    Finally, I need to add some tests.
    
    For the interpreters in general, I should consult the literature to find
    good metrics for describing synteny.

--- R/interpret.R
+++ R/interpret.R
@@ -54,10 +54,31 @@ neighborhood_madness <- function(x, synmap){
 }
 
 #' Summarize the flags returned from a search
-#'  
+#'
+#' Creates a single row for each query, summarizing the set of search intervals
+#' with the following descriptors:
+#'
+#' \enumerate{
+#'   \item inbetween - no search interval overlaps a syntenic block
+#'   \item lo_bound - at least one search interval has a lower overlapping edge
+#'   \item hi_bound - at least one search interval has an upper overlapping edge
+#'   \item doubly_bound - at least one search interval overlaps on both edges
+#'   \item unbound - at least one search interval is unbound on both edges
+#'   \item beyond - at least on search interval is on a target scaffold edge
+#' }
+#'
 #' @param x SearchResult object
 #' @export
 flag_summary <- function(x){
   stopifnot(class(x) == "SearchResult")
-  stop("This function is a stub")
+  as.data.frame(x) %>%
+    dplyr::group_by(.data$attr) %>%
+    dplyr::summarize(
+      inbetween    = all(.data$inbetween),
+      lo_bound     = any(.data$l_flag < 2),
+      hi_bound     = any(.data$r_flag < 2),
+      doubly_bound = any(.data$l_flag < 2 & .data$r_flag < 2),
+      unbound      = all(.data$l_flag > 1 & .data$r_flag > 1),
+      beyond       = any(.data$l_flag == 4 | .data$r_flag == 4)
+    )
 }
